2026f7d87bb9731558410abb2a6dd051fa6d1893
Andreas Ma <andreasma1@outlook.de>
2026-03-02 15:23:48 +0100

    AuthController register
    feat: register endpoint mainly to try out swagger endpoints

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Timebox.Models;
 
 namespace Timebox.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _context;
 
+        public AuthController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            // Return the created user with their new ID
+            return user;
+        }
+    }
 }
